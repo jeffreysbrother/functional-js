c25bc785b5c5126ac2d4d6f2970b55f20ade3b60
James Cool <jeffreysbrother@gmail.com>
2017-04-08 22:46:36 -0700

    functional lecture

---
+++ func.js
@@ -0,0 +1,8 @@
+// Alonzo Church: Lambda Calculus
+// computation expressed in terms of small functions
+// functional programming
+  // pure functions
+  // declarative style programming
+  // predictability
+  // safety: immutable state
+  // modularity: composability, closures
